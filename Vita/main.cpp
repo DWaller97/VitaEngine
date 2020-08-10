3c549c5dcf0d87a41b9224048729573a780ba212
Dan Waller <danwaller97@gmail.com>
2020-08-10 17:37:17 +0100

    Initial Commit

---
+++ Vita/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Vita
+//
+//  Created by Dan Waller on 10/08/2020.
+//  Copyright © 2020 Dan Waller. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
